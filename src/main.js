5eb1da58f544f6a1c74fe107f3154f3bee75a6aa
Jonas <sturmente@proton.me>
2023-05-02 16:30:26 +0200

    Kick banned users who are on a server the bot is running on

--- src/main.js
+++ src/main.js
@@ -9,13 +9,31 @@ const client = new Client({
 
 client.on("ready", () => {
 	console.log("Logged in as", client.user.tag);
+
+	client.guilds.fetch().then((guilds) => {
+		guilds.forEach(async (guild) => {
+			guild.fetch().then((guild) => {
+				guild.members.fetch().then((members) => {
+					members.forEach((member) => {
+						if (config.users.includes(member.id)) {
+							kickMember(member);
+						}
+					});
+				});
+			});
+		});
+	});
 });
 
 client.on("guildMemberAdd", (member) => {
 	if (config.users.includes(member.id)) {
-		member.kick();
-		console.log("Kicked user", member.id, member.user.tag);
+		kickMember(member);
 	}
 });
 
 client.login(process.env.TOKEN);
+
+function kickMember(member) {
+	member.kick();
+	console.log("Kicked user", member.id, `${member.user.tag}`, "from server", member.guild.id, `"${member.guild.name}"`);
+}
